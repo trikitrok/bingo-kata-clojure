760779816d6e140c12b9e1827ea23202fda0ba9e
trikitrok <trikitrok@gmail.com>
2018-03-09 01:37:08 +0100

    checking that all numbers are called

--- test/bingo/caller_test.clj
+++ test/bingo/caller_test.clj
@@ -11,6 +11,11 @@
   (testing "the numbers are between 1 and 75 inclusive and are different each time"
     (caller/reset-numbers-bag!)
     (check (caller/call-number!))
-    (check (caller/call-number!))))
-
+    (check (caller/call-number!)))
+  (testing "after calling number 75 times, all numbers between 1 and 75 are present"
+    (caller/reset-numbers-bag!)
+    (let [called-numbers (atom #{})]
+      (dotimes [_ 75]
+        (swap! called-numbers conj (caller/call-number!)))
+      (is (= @called-numbers (set (range 1 76)))))))
 
