0ced4e55c49b47c7b2ba4087292b0187bfd2b7e9
trikitrok <trikitrok@gmail.com>
2018-03-09 01:28:41 +0100

    removing duplication in tests

--- test/bingo/caller_test.clj
+++ test/bingo/caller_test.clj
@@ -3,14 +3,14 @@
     [clojure.test :refer :all]
     [bingo.caller :as caller]))
 
+(defn- check [number]
+  (is (>= 75 number 1))
+  (is (not (caller/in-bag? number))))
+
 (deftest called-numbers
   (testing "the numbers are between 1 and 75 inclusive and are different each time"
     (caller/reset-numbers-bag!)
-    (let [number (caller/call-number!)]
-      (is (>= 75 number 1))
-      (is (not (caller/in-bag? number))))
-    (let [number (caller/call-number!)]
-      (is (>= 75 number 1))
-      (is (not (caller/in-bag? number))))))
+    (check (caller/call-number!))
+    (check (caller/call-number!))))
 
 
